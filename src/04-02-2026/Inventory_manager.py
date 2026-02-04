0014e09a5d5f68d746da73702c307320e2f08553
SOMASHEKAR N S <somashekar70708@gmail.com>
2026-02-04 10:51:08 +0530

    Delete src/04-02-2026 directory

--- src/04-02-2026/Inventory_manager.py
+++
@@ -1,9 +0,0 @@
-inventory = ["Apples", "Bananas", "Carrots", "Dates"]
-
-print("Current Inventory:", inventory)
-
-inventory.append("Eggs")
-inventory.remove("Bananas")
-inventory.sort()
-
-print("Final Inventory:", inventory)
