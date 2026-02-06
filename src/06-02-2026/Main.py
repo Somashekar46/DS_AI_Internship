6b90e60be301b02d5dc03d075eec7f8e01b1dbdc
Somashekar46 <somashekar70708@gmail.com>
2026-02-06 11:49:18 +0530

    Initial commit

---
+++ src/06-02-2026/Main.py
@@ -0,0 +1,7 @@
+import Math_operations
+
+result_power = Math_operations.power(2, 10)
+result_average = Math_operations.average([10, 20, 30, 40])
+
+print("2^10 =", result_power)
+print("Average =", result_average)
